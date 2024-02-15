71b86c3af44f25b3276e5283b150cf69ec843cc8
matteocicigoi <matteo.cicigoi1@hotmail.it>
2024-02-15 14:57:29 +0100

    feat: axios e componenti (milestone 3)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
 export default {
-  
+    components: {
+        Header,
+        Main
+    }
 }
 </script>
 
 <template>
+    <!-- Header -->
+    <Header></Header>
+    <!-- Fine Header -->
+    <!-- Main -->
+    <Main></Main>
+    <!-- Fine Main -->
 </template>
